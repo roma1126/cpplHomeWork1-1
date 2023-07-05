8c3992072edbc2d2856633f144711a8c931e1d75
Roman <romka200211@gmail.com>
2023-07-05 13:56:39 +0200

    first commit

---
+++ cppl Задача 1. Лямбда - функция.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    std::vector<int> vec{ 4, 7, 9, 14, 12 };
+
+    auto print = [](const int& n)
+    {
+        std::cout << n << " ";
+    };
+
+    std::cout << "Входные данные: ";
+    std::for_each(vec.cbegin(), vec.cend(), print);
+    std::cout << std::endl;
+
+    std::for_each(vec.begin(), vec.end(), [&vec](int& i)
+        {
+            if (i % 2 != 0)
+            {
+                (i) *= 3;
+            }
+        });
+
+    std::cout << "Выходные данные: ";
+    std::for_each(vec.cbegin(), vec.cend(), print);
+    std::cout << std::endl;
+
+}
